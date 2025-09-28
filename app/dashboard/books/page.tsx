ab30db46d7663fdd878f0c4186b5101237243826
Luneroka <luneroka@gmail.com>
2025-09-28 19:53:34 +0200

    feat(frontend): create Books, Lists and AddBooks routes skeletons

---
+++ app/dashboard/books/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Books = () => {
+  return (
+    <div className='flex flex-col gap-16'>
+      <div className='add-item-btn w-fit'>
+        <h3>Ajouter un livre</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Books;
