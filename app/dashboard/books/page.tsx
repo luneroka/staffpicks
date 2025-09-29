89db14188c02b50b3ff27a0f666054eabf7930fa
Luneroka <luneroka@gmail.com>
2025-09-29 12:37:16 +0200

    feat(frontend): display books on the Books page

--- app/dashboard/books/page.tsx
+++ app/dashboard/books/page.tsx
@@ -1,4 +1,7 @@
 import Link from 'next/link';
+import booksData from '../../lib/mock/books.json';
+import Book from '@/app/components/Book';
+import { IoIosArrowForward } from 'react-icons/io';
 
 const Books = () => {
   return (
@@ -8,6 +11,24 @@ const Books = () => {
           <h3>Ajouter un livre</h3>
         </div>
       </Link>
+
+      <div className='flex flex-col gap-8'>
+        <div className='flex gap-2 items-center'>
+          <h2 className='h2-light'>Mes livres</h2>
+          <IoIosArrowForward />
+        </div>
+        {/* Books mapped dynamically from JSON data */}
+        <div id='book-display' className='flex flex-wrap gap-8'>
+          {booksData.map((book) => (
+            <Book
+              key={book._id}
+              coverUrl={book.bookData.cover}
+              isbn={book.isbn}
+              title={book.bookData.title}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
