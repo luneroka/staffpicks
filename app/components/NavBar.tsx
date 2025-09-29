5fb0a2d21c2aa07d4e68990faea3e1b182be8514
Luneroka <luneroka@gmail.com>
2025-09-29 18:05:41 +0200

    refactor(frontend): refactor NavBar nav links styling

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -12,71 +12,6 @@ import {
 const NavBar = () => {
   const pathname = usePathname();
 
-  // Dashboard
-  if (pathname == '/dashboard/books') {
-    return (
-      <nav className='flex justify-between px-16 py-4 border-b border-b-card-background'>
-        {/* LEFT */}
-        <Link href={'/dashboard'}>
-          <div className='flex items-center gap-2 home-link'>
-            {/* TODO: Use company logo from db */}
-            <FaHome />
-            {/* TODO: Use store name from db */}
-            <h3>Genève - Balexert</h3>
-          </div>
-        </Link>
-
-        {/* MIDDLE */}
-        <div className='flex gap-8'>
-          <Link href='/dashboard/lists'>
-            <div className='header-btn flex items-center gap-2'>
-              <FaRegEdit />
-              Mes listes
-            </div>
-          </Link>
-        </div>
-
-        {/* RIGHT */}
-        <div className='flex gap-2 items-center'>
-          <h3>Sarah</h3>
-          <FaUserCircle className='size-8' />
-        </div>
-      </nav>
-    );
-  }
-
-  if (pathname == '/dashboard/lists') {
-    return (
-      <nav className='flex justify-between px-16 py-4 border-b border-b-card-background'>
-        {/* LEFT */}
-        <Link href={'/dashboard'}>
-          <div className='flex items-center gap-2 home-link'>
-            {/* TODO: Use company logo from db */}
-            <FaHome />
-            {/* TODO: Use store name from db */}
-            <h3>Genève - Balexert</h3>
-          </div>
-        </Link>
-
-        {/* MIDDLE */}
-        <div className='flex gap-8'>
-          <Link href='/dashboard/books'>
-            <div className='header-btn flex items-center gap-2'>
-              <FaRegEdit />
-              Mes livres
-            </div>
-          </Link>
-        </div>
-
-        {/* RIGHT */}
-        <div className='flex gap-2 items-center'>
-          <h3>Sarah</h3>
-          <FaUserCircle className='size-8' />
-        </div>
-      </nav>
-    );
-  }
-
   if (
     pathname == '/dashboard/books/new' ||
     pathname == '/dashboard/lists/new'
@@ -125,15 +60,27 @@ const NavBar = () => {
       </Link>
 
       {/* MIDDLE */}
-      <div className='flex gap-8'>
-        <Link href='/dashboard/books'>
-          <div className='header-btn flex items-center gap-2'>
+      <div role='tablist' className='tabs tabs-border'>
+        <Link
+          href='/dashboard/books'
+          role='tab'
+          className={`tab ${
+            pathname === '/dashboard/books' ? 'tab-active' : ''
+          }`}
+        >
+          <div className='flex items-center gap-2'>
             <FaRegEdit />
             Mes livres
           </div>
         </Link>
-        <Link href='/dashboard/lists'>
-          <div className='header-btn flex items-center gap-2'>
+        <Link
+          href='/dashboard/lists'
+          role='tab'
+          className={`tab ${
+            pathname === '/dashboard/lists' ? 'tab-active' : ''
+          }`}
+        >
+          <div className='flex items-center gap-2'>
             <FaRegEdit />
             Mes listes
           </div>
