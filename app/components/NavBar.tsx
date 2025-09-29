3e652d3b283d050ef31e8ae6b113a79799070dcc
Luneroka <luneroka@gmail.com>
2025-09-29 15:57:05 +0200

    feat(frontend): set fall back NavBar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -13,44 +13,6 @@ const NavBar = () => {
   const pathname = usePathname();
 
   // Dashboard
-  if (pathname == '/dashboard') {
-    return (
-      <nav className='flex justify-between px-16 py-4 border-b border-b-card-background'>
-        {/* LEFT */}
-        <Link href={'/dashboard'}>
-          <div className='flex items-center gap-2 home-link'>
-            {/* TODO: Use company logo from db */}
-            <FaHome />
-            {/* TODO: Use store name from db */}
-            <h3>Genève - Balexert</h3>
-          </div>
-        </Link>
-
-        {/* MIDDLE */}
-        <div className='flex gap-8'>
-          <Link href='/dashboard/books'>
-            <div className='header-btn flex items-center gap-2'>
-              <FaRegEdit />
-              Mes livres
-            </div>
-          </Link>
-          <Link href='/dashboard/lists'>
-            <div className='header-btn flex items-center gap-2'>
-              <FaRegEdit />
-              Mes listes
-            </div>
-          </Link>
-        </div>
-
-        {/* RIGHT */}
-        <div className='flex gap-2 items-center'>
-          <h3>Sarah</h3>
-          <FaUserCircle className='size-8' />
-        </div>
-      </nav>
-    );
-  }
-
   if (pathname == '/dashboard/books') {
     return (
       <nav className='flex justify-between px-16 py-4 border-b border-b-card-background'>
@@ -149,6 +111,42 @@ const NavBar = () => {
       </nav>
     );
   }
+
+  return (
+    <nav className='flex justify-between px-16 py-4 border-b border-b-card-background'>
+      {/* LEFT */}
+      <Link href={'/dashboard'}>
+        <div className='flex items-center gap-2 home-link'>
+          {/* TODO: Use company logo from db */}
+          <FaHome />
+          {/* TODO: Use store name from db */}
+          <h3>Genève - Balexert</h3>
+        </div>
+      </Link>
+
+      {/* MIDDLE */}
+      <div className='flex gap-8'>
+        <Link href='/dashboard/books'>
+          <div className='header-btn flex items-center gap-2'>
+            <FaRegEdit />
+            Mes livres
+          </div>
+        </Link>
+        <Link href='/dashboard/lists'>
+          <div className='header-btn flex items-center gap-2'>
+            <FaRegEdit />
+            Mes listes
+          </div>
+        </Link>
+      </div>
+
+      {/* RIGHT */}
+      <div className='flex gap-2 items-center'>
+        <h3>Sarah</h3>
+        <FaUserCircle className='size-8' />
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
