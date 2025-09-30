bb43692e4f4fa9b40bdf584441b31f51efbdf1e1
Luneroka <luneroka@gmail.com>
2025-09-30 14:04:30 +0200

    feat(frontend): add remove icon to Book

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { MdCancel } from 'react-icons/md';
 
 interface BookProps {
   coverUrl?: string;
@@ -7,23 +8,20 @@ interface BookProps {
 }
 
 const Book = ({ coverUrl, isbn, title }: BookProps) => {
-  const bookIsbn = isbn || '1293847562821';
-  const bookCoverUrl =
-    coverUrl ||
-    'https://res.cloudinary.com/dhxckc6ld/image/upload/v1759074962/la_femme_de_menage_hom3dr.webp';
-
   return (
     <div className='flex w-[121px] h-[170px] relative flex-shrink-0 items-center justify-center'>
-      <div className=''>
-        <Link href={`/dashboard/books/${bookIsbn}`}>
+      <div className='hover:scale-105 transition-all duration-200'>
+        <Link href={`/dashboard/books/${isbn}`}>
           <img
-            src={bookCoverUrl}
+            src={coverUrl}
             alt={title || 'Couverture non disponible'}
-            className={`w-full h-full cursor-pointer hover:scale-105 transition-all duration-200
-            }`}
+            className='w-full h-full cursor-pointer'
             style={{ width: '121px', height: '170px' }}
           />
         </Link>
+        <button className='absolute top-1 right-1 rounded-full hover:text-primary-btn text-error cursor-pointer hover:scale-150 transition-all duration-200'>
+          <MdCancel className='size-5' />
+        </button>
       </div>
     </div>
   );
