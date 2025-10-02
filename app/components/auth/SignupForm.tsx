81ee3a30c89a82a1fc7c903ac806bb0ef3ddee2e
Luneroka <luneroka@gmail.com>
2025-10-02 11:23:41 +0200

    feat(frontend): conditional rendering of input validation

--- app/components/auth/SignupForm.tsx
+++ app/components/auth/SignupForm.tsx
@@ -1,6 +1,10 @@
 'use client';
 
+import { useState } from 'react';
+
 const SignupForm = () => {
+  const [passwordValue, setPasswordValue] = useState('');
+
   return (
     <form action=''>
       <fieldset className='fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4'>
@@ -8,7 +12,7 @@ const SignupForm = () => {
           Informations professionelles
         </legend>
 
-        <label className='label'>Nom de l'entreprise</label>
+        <label className='label'>Nom de l'entreprise *</label>
         <input
           type='text'
           className='input'
@@ -22,13 +26,13 @@ const SignupForm = () => {
       <fieldset className='fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4'>
         <legend className='fieldset-legend'>Informations personnelles</legend>
 
-        <label className='label'>Votre nom</label>
+        <label className='label'>Votre nom *</label>
         <input type='email' className='input' placeholder='Votre nom' />
 
-        <label className='label'>Email</label>
+        <label className='label'>Email *</label>
         <input type='email' className='input' placeholder='Adresse email' />
 
-        <label className='label'>Mot de passe</label>
+        <label className='label'>Mot de passe *</label>
         <input
           type='password'
           className='input validator'
@@ -37,16 +41,27 @@ const SignupForm = () => {
           minLength={8}
           pattern='(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}'
           title='Must be more than 8 characters, including number, lowercase letter, uppercase letter'
+          value={passwordValue}
+          onChange={(e) => setPasswordValue(e.target.value)}
         />
-        <p className='validator-hint'>
-          Doit contenir plus de 8 caractères, incluant :
-          <br />
-          Au moins un chiffre
-          <br />
-          Au moins une lettre minuscule
-          <br />
-          Au moins une lettre majuscule
-        </p>
+        <label className='label'>Confirmer mot de passe *</label>
+        <input
+          type='password'
+          className='input'
+          required
+          placeholder='Confirmer mot de passe'
+        />
+        {passwordValue.length > 0 && (
+          <p className='validator-hint'>
+            Doit contenir plus de 8 caractères, incluant :
+            <br />
+            Au moins un chiffre
+            <br />
+            Au moins une lettre minuscule
+            <br />
+            Au moins une lettre majuscule
+          </p>
+        )}
       </fieldset>
       <button className='btn btn-soft btn-primary mt-4 w-full'>
         Créer mon compte
